94c8f5ecdd47159d865282f1bfd81bf1eaed27ca
Hernand Azevedo <hernand.azevedo@zup.com.br>
2022-03-22 15:32:21 -0300

    Complex Layouts

--- app/src/main/java/com/hernandazevedo/composelayouts/MainActivity.kt
+++ app/src/main/java/com/hernandazevedo/composelayouts/MainActivity.kt
@@ -3,29 +3,13 @@ package com.hernandazevedo.composelayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.hernandazevedo.composelayouts.ui.theme.ComposeLayoutsTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +35,6 @@ fun ChipPreview() {
     }
 }
 
-
 @Preview
 @Composable
 fun LayoutsCodelabPreviewStaggeredGrid() {
@@ -59,7 +42,6 @@ fun LayoutsCodelabPreviewStaggeredGrid() {
         BodyContentStaggeredGrid()
     }
 }
-
 
 @Preview(showBackground = true, widthDp = 300, heightDp = 300)
 @Composable
@@ -85,3 +67,26 @@ fun TextWithNormalPaddingPreview() {
     }
 }
 
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    ComposeLayoutsTheme {
+        ConstraintLayoutContent()
+    }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    ComposeLayoutsTheme {
+        LargeConstraintLayout()
+    }
+}
+
+@Preview
+@Composable
+fun DecoupledConstraintLayoutPreview() {
+    ComposeLayoutsTheme {
+        DecoupledConstraintLayout()
+    }
+}
